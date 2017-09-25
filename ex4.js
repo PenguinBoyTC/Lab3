7b9b4e9f2ad11b60712575780a184277a3c5a522
Chong Tan <a46981@gmail.com>
2017-09-25 01:20:01 -0500

    add changecolor function

---
+++ ex4.js
@@ -0,0 +1,6 @@
+function changeborder() {
+  document.getElementById("Border").style.border = "thick solid red";
+}
+function Background() {
+  document.getElementById("Border").style.backgroundColor = "blue";
+}
